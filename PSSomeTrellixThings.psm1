12c9145b403ef8760bd024729d9f5b7fc458f543
qqt-lo4 <ade.loic@gmail.com>
2026-03-04 22:33:23 +0100

    Updated psm1 file to follow junctions

--- PSSomeTrellixThings.psm1
+++ PSSomeTrellixThings.psm1
@@ -1,5 +1,5 @@
 ﻿# Load all .ps1 functions from the module directory and subdirectories
-foreach ($Script in (Get-ChildItem -Path $PSScriptRoot -Filter '*.ps1' -File -Recurse)) {
+foreach ($Script in (Get-ChildItem -Path $PSScriptRoot -Filter '*.ps1' -File -Recurse -FollowSymlink)) {
     try {
         . $Script.FullName
     } catch {
